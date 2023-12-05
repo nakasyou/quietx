a5127bde7208d25f37a4c40f016d9cb66c93bcaf
nakasyou <syoutarou1103@outlook.jp>
2023-12-05 15:42:38 +0900

    fix(blocklist): 追加

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,13 +1,17 @@
 import { mkext } from './mkext.ts'
 
-await mkext() // First build
-console.log('ended')
-
-for await (const _evt of Deno.watchFs('./extension', { recursive: true })) {
-  try {
-    await mkext()
-  } catch (error) {
-    console.error(error)
-  }
-  console.log('ended')
-}
+await Promise.all([
+  (async () => {
+    await mkext() // First build
+    console.log('ended')
+    
+    for await (const _evt of Deno.watchFs('./extension', { recursive: true })) {
+      try {
+        await mkext()
+      } catch (error) {
+        console.error(error)
+      }
+      console.log('ended')
+    }    
+  })()
+])
